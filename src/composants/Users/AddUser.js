a66224d7c2dd4e00beebd8e1a4ac756716964b77
Amine MEBARKI <medaminemebarki@gmail.com>
2022-07-06 01:04:44 +0200

    added 'user' component with all its logic

---
+++ src/composants/Users/AddUser.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css'
+import Button from '../UI/Button';
+const AddUser = (props) => {
+    const [username,setusername] = useState('');
+    const [age,setage] = useState(''); 
+    const recupdata = (event) => {
+        event.preventDefault();
+        if(username.trim().length === 0 || age.trim().length === 0) return;
+        if(+age < 0) return;
+        console.log(username, age);
+        setusername('');
+        setage('');
+    }
+   
+    const entredusername = (event) => {
+        setusername(event.target.value);
+    }
+    const entredage = (event) => {
+        setage(event.target.value);
+    }
+return (<Card className={classes.input}>
+        <form onSubmit={recupdata}>
+        
+                <label htmlFor='username'>Username : </label>
+                <input id='username' type='text' value={username} onChange={entredusername}/>
+                <label htmlFor='age'>Age (Years) : </label>
+                <input id='age' type='number' value={age} onChange={entredage}/>
+                 <Button type='submit'>Add User</Button>
+        </form>
+        </Card>
+    )
+};
+
+export default AddUser;
