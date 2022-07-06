8b44e67753b721c15eba88e3a8d78d53197e030f
Amine MEBARKI <medaminemebarki@gmail.com>
2022-07-06 12:52:14 +0200

    added users list component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AddUser from './composants/Users/AddUser';
+import UsersList from './composants/Users/UsersList';
 function App() {
  return (
   <div>
 <AddUser />
+<UsersList users={[]}/>
   </div>
  )
 
